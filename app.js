7f6c5ed1338eb887c7c1ad3cb348fdd7f529738a
Nasar Issa <nkissa@ucsd.edu>
2019-02-07 22:03:45 -0800

    Made initial changes, connected routes to home and second screen

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var project = require("./routes/project");
 // Example route
 // var user = require('./routes/user');
 
@@ -35,6 +36,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', index.view);
+app.get("/project/:name",project.viewProject);
+app.get('/index',index.viewProject);
 // Example route
 // app.get('/users', user.list);
 
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,31 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index',{
+  	"nav_title": [
+  	{
+  		"name": "My Workouts",
+  		"id": "nav1"
+  	},
+  	{
+  		"name": "Timer",
+  		"id": "nav2"
+  	},
+  	{
+  		"name": "Music",
+  		"id": "nav3"
+  	},
+  	{
+  		"name": "Help and Documentation",
+  		"id": "nav3"
+  	}
+  	]
+  });
 };
+
+exports.viewProject = function(req, res){
+  var name = req.params.name;
+  console.log("The project name is: " + name);
+  res.render(index);
+}
+
