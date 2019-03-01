aedf9cf745558c01e74e02e3eb5f2e8d10195c3a
Nasar Issa <nkissa@ucsd.edu>
2019-02-28 22:50:26 -0800

    Made Design changes for an alternative index

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var bodyParser = require("body-parser");
 
 var index = require('./routes/index');
 var login = require("./routes/login");
+var altIndex = require("./routes/altbIndex");
 var help = require("./routes/help");
 var add = require("./routes/add");
 var timer = require("./routes/timer");
@@ -43,6 +44,7 @@ if ('development' == app.get('env')) {
 
 app.get("/", login.viewLogin);
 app.get('/index', index.view);
+app.get('/altindex', altIndex.view);
 app.get("/help", help.viewHelpHB);
 app.get("/add", add.viewAddHB);
 app.get("/getdata", data.getData);
