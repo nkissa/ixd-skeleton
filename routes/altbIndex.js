b9332d30f57e1ae851805f0190b16b705ae37f62
Nasar Issa <nkissa@ucsd.edu>
2019-02-28 23:01:49 -0800

    Made more design changes for altbindex

--- routes/altbIndex.js
+++ routes/altbIndex.js
@@ -5,5 +5,5 @@
 var data = require('../data.json');
 
 exports.view = function(req, res){
-  res.render("altbindex", { "exercise": data.exercises[id], "Nav": data.Nav });
+  res.render("altbindex", data);
 };
